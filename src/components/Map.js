53fa34d112fdd57e4ca669d7fdce388db9fee4e8
HyeinKang <hyein@agencyleroy.com>
2020-05-21 12:24:41 +0300

    Add GeolocateControl, bit of styling, Add indicator for the stations

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import _ from 'lodash';
 
-import ReactMapboxGl, { Layer, Feature, Popup } from 'react-mapbox-gl';
+import { GeolocateControl } from "mapbox-gl";
+import ReactMapboxGl, { Layer, Feature, Popup, ZoomControl } from 'react-mapbox-gl';
 
 const MapboxMap = ReactMapboxGl({
   accessToken: 'pk.eyJ1IjoiaGVpbmEta2FuZyIsImEiOiJja2FhNGppb2IwdDNqMnZxd3h1bnFja2NhIn0.si285aYkKGq4jcOjvpryzw',
@@ -14,7 +15,8 @@ class Map extends React.Component {
 
     this.state = {
       center: [24.964421, 60.197636],
-      zoom: [15]
+      zoom: [15],
+      maxZoom: [18]
     }
   }
 
@@ -26,16 +28,34 @@ class Map extends React.Component {
 
   render() {
     const { updateBounds, locations } = this.props;
+    const onMapLoad = map => { map.addControl(new GeolocateControl()); };
+
+    function StationsInfo({location}) {
+      const stationCapability = location.bikesAvailable + location.spacesAvailable;
+      const bikeAvailability = location.bikesAvailable;
+
+      const bikePercentage = bikeAvailability / stationCapability * 100;
+      const indicator = bikePercentage > 50 ? "safe" : bikePercentage > 0 ? "warning" : "bad";
+
+      return (
+        <Popup coordinates={[location.lon, location.lat]} key={location.stationId} >
+          <div className="station-name"><span>{location.name}</span> <div className={`${indicator} indicator`}></div></div>
+          <div>{bikeAvailability}<span className="divider">/</span>{stationCapability}</div>
+        </Popup>
+      )
+    }
     return (
       <div>
         <MapboxMap
             style="mapbox://styles/mapbox/streets-v8"
             center={this.state.center}
             zoom={this.state.zoom}
+            maxZoom={this.state.maxZoom}
             containerStyle={{
               width: '100vw',
               height: '100vh'
             }}
+            onStyleLoad={onMapLoad}
             onRender={(map) => { updateBounds(map.getBounds()) }}
           >
             <Layer type="symbol" id="marker" layout={{ 'icon-image': 'bicycle-15' }}>
@@ -44,11 +64,9 @@ class Map extends React.Component {
               ))}
             </Layer>
           { locations.map((location) => (
-            <Popup coordinates={[location.lon, location.lat]} key={location.stationId} >
-              <div>{location.name}</div>
-              <div>{location.bikesAvailable}/{location.bikesAvailable + location.spacesAvailable}</div>
-            </Popup>
+            <StationsInfo key={location.stationId} location={location} />
           ))}
+          <ZoomControl position="bottom-right" />
         </MapboxMap>
       </div>
     );
