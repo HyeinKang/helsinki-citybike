bfd842765aaab2656bf03966e58fc59401064f27
HyeinKang <hyein@agencyleroy.com>
2020-06-12 04:58:52 +0300

    Configure docker-compose with docker containers for the app, the api, mongoDB, mondgo-express

--- api/app.js
+++ api/app.js
@@ -9,8 +9,6 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.listen(8081);
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
