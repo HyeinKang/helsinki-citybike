f84d30be3269d7fab284a03461c74bcf6d3f4192
HyeinKang <hyein@agencyleroy.com>
2020-06-24 02:59:26 +0300

    Update .env

--- api/model.js
+++ api/model.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 require('dotenv').config({ path: '../.env' })
 
 //Set up default mongoose connection
-var mongoDB = `mongodb://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@mongo:27017/${process.env.MONGO_DB}`;
+var mongoDB = `mongodb://${process.env.STATION_DB_USER}:${process.env.STATION_DB_PASSWORD}@mongo:27017/${process.env.STATION_DB}`;
 console.log('mongoDB: ', mongoDB);
 mongoose.connect(mongoDB, { useNewUrlParser: true })
    .then(() =>  console.log('connection successful'))
