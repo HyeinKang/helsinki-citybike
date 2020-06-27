8020e7bff3f62986fb123dd292a1540d23bb9e34
HyeinKang <hyein@agencyleroy.com>
2020-06-28 00:49:47 +0300

    api remove consoles

--- api/model.js
+++ api/model.js
@@ -4,9 +4,8 @@ require('dotenv').config({ path: '../.env' })
 
 //Set up default mongoose connection
 var mongoDB = `mongodb://${process.env.STATION_DB_USER}:${process.env.STATION_DB_PASSWORD}@mongo:27017/${process.env.STATION_DB}`;
-console.log('mongoDB: ', mongoDB);
 mongoose.connect(mongoDB, { useNewUrlParser: true })
    .then(() =>  console.log('connection successful'))
-   .catch((err) => console.error(err));;
+   .catch((err) => console.error(err));
 
 module.exports = mongoose;
