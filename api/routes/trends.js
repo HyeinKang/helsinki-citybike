dd08e882b5bac7d801b400aef225d601fb364a2f
HyeinKang <hyein@agencyleroy.com>
2020-07-10 05:12:26 +0300

    Get trends by station Id

--- api/routes/trends.js
+++ api/routes/trends.js
@@ -1,14 +1,25 @@
+
+var _ = require('lodash/core');
 var express = require('express');
 var router = express.Router();
 
 var Trend = require('../models/trends');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/:id', function(req, res, next) {
   res.send('respond with a resource');
 
-  Trend.find({}, function(err, trends) {
+  Trend.find({ "stationId": req.params.id}, function(err, trends) {
     console.log('trends', trends);
+
+    // TODO: Get all collections with the statioinId
+    // TODO: Group collections by hour
+    // TODO: In each hour, get length of availbableBikes key-value pair
+    // TODO: Find the average available bikes by summing all availableBikes / length of the pair
+    // TODO: Return average bike trends per hour by bikeStationId
+
+    // var test = _.map(trends, 'availableBikes');
+    // console.log('test: ', test);
   });
 
   // res.json({ firstname: 'John', lastname: 'Doe' });
