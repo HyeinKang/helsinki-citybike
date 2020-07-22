8e33c3d56c90717b1a0fbcd665194c7791ae4022
HyeinKang <hyein@agencyleroy.com>
2020-07-23 00:15:00 +0300

    Changing direction to popover instead of accordion to display station information

--- app/src/components/StationPopover.js
+++ app/src/components/StationPopover.js
@@ -46,16 +46,13 @@ class StationPopover extends React.Component {
           <div className="station-name"><span>{this.props.name}</span> <div className={`${this.props.indicator}  ${this.props.isLoading ? "is-loading" : ""} indicator`}></div></div>
           <div className="station-availity">
             <div className={`avilability`}>{this.props.bikeAvailability}<span className="divider">/</span>{this.props.stationCapability}</div>
-            <div>
+            <div className="more-info">
               <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
-                <path d="M0 0h24v24H0V0z" fill="none"/>
-                <path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"/>
+                <path d="M0 0h24v24H0V0z" fill="none" />
+                <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8-8-8z" />
               </svg>
             </div>
           </div>
-        </div>
-        <div className="station-more">
-          Graph
         </div>
       </div>
     );
