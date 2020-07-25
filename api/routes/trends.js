58c1a18cf80443e7c10ac6929d2c34ef01ed1b91
HyeinKang <hyein@agencyleroy.com>
2020-07-25 04:04:18 +0300

    Update trend API to return bike trends based on days

--- api/routes/trends.js
+++ api/routes/trends.js
@@ -1,25 +1,29 @@
 
 var _ = require('lodash');
-var moment = require('moment');
+var moment = require('moment-timezone');
 var express = require('express');
 var router = express.Router();
 
 var Trend = require('../models/trends');
 
 /* GET available bikes by hour. */
-router.get('/:id', function(req, res, next) {
+router.get('/:id/:dayNumber', function(req, res, next) {
   // res.send('respond with a resource');
-  var averageBikesByHour = {};
+  var averageBikesByHour = [];
 
   Trend.find({ "stationId": req.params.id}, function(err, trends) {
-    var groupByHour = _.groupBy(trends, (dt) => moment(dt.dateTime).hour());
+    var groupByDay = _.groupBy(trends, (dt) => moment(dt.dateTime).tz("Europe/Helsinki").day());
+    var today = moment().tz("Europe/Helsinki").day();
+    var groupByHour = _.groupBy(groupByDay[req.params.dayNumber||today], (dt) => moment(dt.dateTime).tz("Europe/Helsinki").hour());
     _.forEach(groupByHour, (group, hour) => {
       var bikesAvailableArray = _.map(group, 'bikesAvailable');
       var allBikes = _.reduce(bikesAvailableArray, function(sum, n) {
         return sum + n;
       }, 0);
       var averageBikesAvailable = Math.round(allBikes/group.length);
-      averageBikesByHour[hour] = averageBikesAvailable;
+      // averageBikesByHour[hour] = averageBikesAvailable;
+      var formattedHour = moment(hour,'HH').format('ha'); // 21:00
+      averageBikesByHour = [...averageBikesByHour,{'time': formattedHour, 'averageBikesAvailable': averageBikesAvailable}]
     })
     res.json(averageBikesByHour);
   });
