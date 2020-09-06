ee2a92d51d33d2632f37a870213a2bf523e01453
HyeinKang <hyein@agencyleroy.com>
2020-09-07 01:15:34 +0300

    Build files

---
+++ app/build/precache-manifest.88e3a82432e557fc0b1eb73b6ee085c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0a6139ea3736e307fe9a815041ea90a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cb64d2a6e4dbc1b3ada",
+    "url": "/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "de79f97e8c664ee64190",
+    "url": "/static/css/main.1980636a.chunk.css"
+  },
+  {
+    "revision": "6cb64d2a6e4dbc1b3ada",
+    "url": "/static/js/2.e1ab1798.chunk.js"
+  },
+  {
+    "revision": "c883f390cc4a2cca44d47e9878428785",
+    "url": "/static/js/2.e1ab1798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de79f97e8c664ee64190",
+    "url": "/static/js/main.26380628.chunk.js"
+  },
+  {
+    "revision": "39ce0cc8074365f9c7ce",
+    "url": "/static/js/runtime-main.5b29fcae.js"
+  }
+]);
