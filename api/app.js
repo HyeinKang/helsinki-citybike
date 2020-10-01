9c78884f019eef8c217efb04648ea6737571d449
HyeinKang <hyein@agencyleroy.com>
2020-10-02 01:47:51 +0300

    Add timeout for api

--- api/app.js
+++ api/app.js
@@ -11,7 +11,11 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something went wrong!')
+})
 
 app.use('/api/trends', trendsRouter)
 
--- api/routes/trends.js
+++ api/routes/trends.js
@@ -7,26 +7,31 @@ const Trend = require('../models/trends')
 const today = moment().tz("Europe/Helsinki").day()
 
 router.get('/:id/:dayNumber', function(req, res, next) {
-  Trend.find({ "stationId": req.params.id }, function(err, availableBikesByStation) {
-    const selectedDayNumber = req.params.dayNumber
-    const availableBikesByDay = _.groupBy(availableBikesByStation, (dt) => moment(dt.dateTime).tz("Europe/Helsinki").day())
-    const availableBikesByHour = _.groupBy(availableBikesByDay[selectedDayNumber||today], (dt) => moment(dt.dateTime).tz("Europe/Helsinki").hour())
-    let averageBikesByHour = []
+  Trend
+    .find({ "stationId": req.params.id })
+    .maxTime(10000)
+    .exec((err, availableBikesByStation) => {
+      if (err) return next(err);
 
-    _.forEach(availableBikesByHour, (bikes, hour) => {
-      const allBikesAmount = _.reduce(_.map(bikes, 'bikesAvailable'), (sum, n) => {
-        return sum + n
-      }, 0)
-      const averageBikesAvailable = Math.round(allBikesAmount/bikes.length)
-      const formattedHour = moment(hour,'HH').format('ha') // 21:00
-      const averageBikes = {
-        'time': formattedHour,
-        'averageBikesAvailable': averageBikesAvailable
-      }
-      averageBikesByHour = [...averageBikesByHour, averageBikes]
+      const selectedDayNumber = req.params.dayNumber
+      const availableBikesByDay = _.groupBy(availableBikesByStation, (dt) => moment(dt.dateTime).tz("Europe/Helsinki").day())
+      const availableBikesByHour = _.groupBy(availableBikesByDay[selectedDayNumber||today], (dt) => moment(dt.dateTime).tz("Europe/Helsinki").hour())
+      let averageBikesByHour = []
+
+      _.forEach(availableBikesByHour, (bikes, hour) => {
+        const allBikesAmount = _.reduce(_.map(bikes, 'bikesAvailable'), (sum, n) => {
+          return sum + n
+        }, 0)
+        const averageBikesAvailable = Math.round(allBikesAmount/bikes.length)
+        const formattedHour = moment(hour,'HH').format('ha') // 21:00
+        const averageBikes = {
+          'time': formattedHour,
+          'averageBikesAvailable': averageBikesAvailable
+        }
+        averageBikesByHour = [...averageBikesByHour, averageBikes]
+      })
+      res.json(averageBikesByHour)
     })
-    res.json(averageBikesByHour)
-  })
 })
 
 module.exports = router
