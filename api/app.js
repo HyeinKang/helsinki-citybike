07f613ac388713f9f18e50264659e210c99f3b08
HyeinKang <hyein@agencyleroy.com>
2020-10-02 00:09:54 +0300

    Remove unused features

--- api/app.js
+++ api/app.js
@@ -3,7 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var stationsRouter = require('./routes/stations');
 var trendsRouter = require('./routes/trends');
 
 var app = express();
@@ -14,7 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/stations', stationsRouter);
 app.use('/api/trends', trendsRouter);
 
 module.exports = app;
